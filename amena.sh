552ce9ab09565f70ee6190f35a4892d8e3e73cd5
amenakun <50862294+amenakun@users.noreply.github.com>
2019-05-27 16:17:40 +0700

    add files via upload

---
+++ amena.sh
@@ -0,0 +1,136 @@
+clear
+figlet SANTRI | lolcat
+
+echo "======================================
+
+# CREATED BY : RO0T 4M3NA
+# DOMISILI   : PASURUAN
+# FACEBOOK   : Amena Gans
+# CONTACT ME : 089530614080
+
+============================================" | lolcat
+
+echo "[1] remote hp orang
+[2] Hack Facebook Pakistan
+[3] Lacak IP
+[4] Matrix
+[5] bot facebook
+[6] Hack akun ML
+[7] Buat script deface
+[8] Bom Gmail
+[9] Crack Hash Pasword
+[10] Install Ubuntu
+[11] hack WI-FI (root)
+[00] Exit" | lolcat
+echo "pilih nomernya beb:  " | lolcat
+read nomor
+
+if [$nomor = 1 ]
+then
+clear
+apt install git
+git clone https://github.com/Xi4u7/A-Rat.git
+cd A-Rat
+python2 A-Rat.py
+fi
+
+if [ $nomor = 2 ]
+then
+clear
+pkg install git -y
+git clone https://github.com/nasirxo/flb
+pip2 install mechanize
+pip2 install requests
+cd flb
+chmod +x flb.py
+python2 flb.py
+fi
+
+if [ $nomor = 3 ]
+then
+clear
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+chmod +x ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py
+fi
+
+if [ $nomor = 4 ]
+then
+clear
+pkg install cmatrix
+cmatrix
+fi
+
+if [ $nomor = 5 ]
+then
+clear
+git clone https://github.com/kereh/BOT
+ls
+cd BOT
+python2 blackbot.py
+fi
+
+if [ $nomor = 6 ]
+then
+clear
+git clone https://github.com/Senitopeng/PhisingGame.git
+cd PhisingGame
+python2 phising.py
+fi
+
+if [ $nomor = 7 ]
+then
+clear
+git clone https://github.com/4L13199/LITESCRIPT
+ls
+cd LITESCRIPT
+ls
+python2 LITESCRIPT.py
+fi
+
+if [ $nomor = 8 ]
+then
+clear
+git clone https://github.com/root-ID-041/bom-mail
+cd bom-mail
+python2 Bom-mail.py
+fi
+
+if [ $nomor = 9 ]
+then
+clear
+git clone https://github.com/ UltimateHackers/Hash-Buster
+cd Hash-Buster
+chmod 777 hash.py
+python2 hash.py
+fi
+
+if [ $nomor = 10 ]
+then
+clear
+apt install wget
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+chmod +x ubuntu.sh
+./ubuntu.sh
+./start-ubuntu.sh
+fi
+
+if [ $nomor = 11 ]
+then
+clear
+apt-get install git
+apt-get install python python-pip python-setuptools
+pip install scapy
+git clone https://github.com/wifiphisher/wifiphisher.git
+cd wifiphisher
+python setup.py install
+fi
+
+if [ $nomor = 00 ]
+then
+clear
+exit
+fi
